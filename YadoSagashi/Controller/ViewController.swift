b8e8ab757061a1e0f0387da39f85995422d7272c
yu20201227 <yasada32020@gmail.com>
2020-12-11 15:34:09 +0900

    アニメーション追加

--- YadoSagashi/Controller/ViewController.swift
+++ YadoSagashi/Controller/ViewController.swift
@@ -12,11 +12,27 @@ import SwiftyJSON
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
     
+    var textField:UITextField!
+    var mapView:MKMapView!
     
+    var animationView = AnimationView()
+    var locationManager = CLLocationManager()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func startAnimation(){
+        animationView = AnimationView()
+        let animation = Animation.named("1.json")
+        animationView.animation = animation
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 1.0
+        animationView.play()
+        view.addSubview(animationView)
     }
 
 
